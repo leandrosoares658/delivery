feede2fc4b2e3c38f016e74f47d278f243ce5edd
Leandro Soares <leandrosoares658@gmail.com>
2021-02-14 05:06:13 -0300

    Ready to prod

--- backend/src/main/java/com/leandrosoares658/delivery/services/OrderService.java
+++ backend/src/main/java/com/leandrosoares658/delivery/services/OrderService.java
@@ -45,4 +45,12 @@ public class OrderService {
 		order = repository.save(order);
 		return new OrderDTO(order);
 	}
+	
+	@Transactional
+	public OrderDTO sertDelivered(Long Id) {
+		Order order = repository.getOne(Id);
+		order.setStatus(OrderStatus.DELIVERED);
+		order = repository.save(order);
+		return new OrderDTO(order);
+	}
 }
